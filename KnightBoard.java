b56cb3be38afc465cdd58c7ee1809713b17f4f7e
Julian Wong <jwong01@stuy.edu>
2019-02-26 19:18:25 -0500

    countsolutions finds one solution, i don't know how to code the backtracking

--- KnightBoard.java
+++ KnightBoard.java
@@ -62,6 +62,49 @@ public class KnightBoard {
       return false;
     }
   }
+  public int countSolutions(int r, int c) {
+    if(bnc()) {
+      throw new IllegalStateException("board isn't clear");
+    }
+    if(r < 0 || c < 0) {
+      throw new IllegalArgumentException("row and col params can't be negative");
+    }
+    if(r >= board.length) {
+      throw new IllegalArgumentException("row param exceeds row dimension");
+    }
+    if(c >= board[0].length) {
+      throw new IllegalArgumentException("col param exceeds col dimension");
+    }
+    return csh(r, c, 1);
+  }
+  public int csh(int r, int c, int mn) {
+    if(r < 0 || c < 0 || r >= board.length || c >= board[0].length) {
+      return 0;
+    }
+    else if(board[r][c] != 0) {
+      return 0;
+    }
+    else if(mn >= board.length * board[0].length) {
+      board[r][c] = mn;
+      return 1;
+    }
+    else {
+      board[r][c] = mn;
+      int sum =
+      csh(r + 1, c - 2, mn + 1) +
+      csh(r + 2, c - 1, mn + 1) +
+      csh(r + 2, c + 1, mn + 1) +
+      csh(r + 1, c + 2, mn + 1) +
+      csh(r - 1, c + 2, mn + 1) +
+      csh(r - 2, c + 1, mn + 1) +
+      csh(r - 2, c - 1, mn + 1) +
+      csh(r - 1, c - 2, mn + 1);
+      if(sum == 0) {
+        board[r][c] = 0;
+      }
+      return sum;
+    }
+  }
   public boolean bnc() {
     for(int q = 0; q < board.length; q++) {
       for(int w = 0; w < board[q].length; w++) {
@@ -73,8 +116,9 @@ public class KnightBoard {
     return false;
   }
   public static void main(String[] args) {
-    KnightBoard test = new KnightBoard(6,6);
-    System.out.println(test.solve(0,0));
+    KnightBoard test = new KnightBoard(8,8);
+    //System.out.println(test.solve(0,0));
+    System.out.println(test.countSolutions(0,0));
     System.out.println(test);
   }
 }
